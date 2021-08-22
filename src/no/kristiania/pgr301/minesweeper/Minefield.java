a9306ad8741138ef346816cd241710838af07c48
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-08-22 21:59:03 +0200

    Add comments (and revert alternative implementation)

--- src/no/kristiania/pgr301/minesweeper/Minefield.java
+++ src/no/kristiania/pgr301/minesweeper/Minefield.java
@@ -1,5 +1,11 @@
 package no.kristiania.pgr301.minesweeper;
 
+/**
+ * Represents a minefield for the game Mine Sweeper. The board is a
+ * rectagonal grid of cells that either have a mine on it or not.
+ * For cells that don't have a mine, the minefield calculates a hint
+ * with the number of neighbouring cells that have mines on them
+ */
 public class Minefield {
     private final String[] input;
 
@@ -9,6 +15,7 @@ public class Minefield {
 
     public String[] getHints() {
         String[] result = new String[input.length];
+        // Go through each row and column and calculate what value should be in the cell
         for (int row = 0, inputLength = input.length; row < inputLength; row++) {
             StringBuilder line = new StringBuilder();
             for (int col = 0; col < input[row].length(); col++) {
@@ -22,20 +29,21 @@ public class Minefield {
     private String calculateCellValue(int row, int col) {
         if (hasMine(row, col)) {
             return "*";
+        } else {
+            return String.valueOf(countMinesAroundCell(row, col));
         }
-        return String.valueOf(countMinesAroundCell(row, col));
     }
 
     private int countMinesAroundCell(int row, int col) {
         int neighbourMines = 0;
-        if (hasMine(row-1, col-1)) neighbourMines++;
-        if (hasMine(row-1, col)) neighbourMines++;
-        if (hasMine(row-1, col+1)) neighbourMines++;
-        if (hasMine(row, col-1)) neighbourMines++;
-        if (hasMine(row, col+1)) neighbourMines++;
-        if (hasMine(row+1, col-1)) neighbourMines++;
-        if (hasMine(row+1, col)) neighbourMines++;
-        if (hasMine(row+1, col+1)) neighbourMines++;
+        // Count number of cells with mines from row - 1 to row + 1 and col - 1 to col +1
+        for (int r = row -1; r <= row +1; r++) {
+            for (int c = col -1; c <= col +1; c++) {
+                if (hasMine(r, c)) {
+                    neighbourMines += 1;
+                }
+            }
+        }
         return neighbourMines;
     }
 
