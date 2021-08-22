4061b934b4e92159a7bfffdf45a04a4929a11f19
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-08-22 21:22:40 +0200

    hintsShouldReflectInputBoardSize (passing)

--- src/no/kristiania/pgr301/minesweeper/Minefield.java
+++ src/no/kristiania/pgr301/minesweeper/Minefield.java
@@ -1,10 +1,17 @@
 package no.kristiania.pgr301.minesweeper;
 
 public class Minefield {
+    private final String[] input;
+
     public Minefield(String[] input) {
+        this.input = input;
     }
 
     public String[] getHints() {
-        return new String[] { "0" };
+        String[] result = new String[input.length];
+        for (int i = 0, inputLength = input.length; i < inputLength; i++) {
+            result[i] = "0";
+        }
+        return result;
     }
 }
