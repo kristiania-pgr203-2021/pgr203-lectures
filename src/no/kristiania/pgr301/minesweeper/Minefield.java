b7aae93edb3cc1534ff75208ce951d51dd987c5c
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-08-22 21:49:36 +0200

    shouldCountMinesAroundCell (refactor step 4)

--- src/no/kristiania/pgr301/minesweeper/Minefield.java
+++ src/no/kristiania/pgr301/minesweeper/Minefield.java
@@ -22,17 +22,20 @@ public class Minefield {
     private String calculateCellValue(int row, int col) {
         if (hasMine(row, col)) {
             return "*";
-        } else {
-            int neighbourMines = 0;
-            for (int r = row -1; r <= row +1; r++) {
-                for (int c = col -1; c <= col +1; c++) {
-                    if (hasMine(r, c)) {
-                        neighbourMines += 1;
-                    }
+        }
+        return String.valueOf(countMinesAroundCell(row, col));
+    }
+
+    private int countMinesAroundCell(int row, int col) {
+        int neighbourMines = 0;
+        for (int r = row -1; r <= row +1; r++) {
+            for (int c = col -1; c <= col +1; c++) {
+                if (hasMine(r, c)) {
+                    neighbourMines += 1;
                 }
             }
-            return String.valueOf(neighbourMines);
         }
+        return neighbourMines;
     }
 
     private boolean hasMine(int row, int col) {
