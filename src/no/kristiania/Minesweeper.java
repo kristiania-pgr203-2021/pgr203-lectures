b55ed492ce873f36f3a7e874c46fe2d60878c757
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-08-26 16:02:07 +0200

    shouldShowTallMinefields (passes)

--- src/no/kristiania/Minesweeper.java
+++ src/no/kristiania/Minesweeper.java
@@ -1,11 +1,17 @@
 package no.kristiania;
 
 public class Minesweeper {
+    private final String[] field;
+
     public Minesweeper(String[] field) {
+        this.field = field;
     }
 
     public String[] getHints() {
-        String[] expected = { "0" };
+        String[] expected = new String[field.length];
+        for (int i = 0; i < field.length; i++) {
+            expected[i] = "0";
+        }
         return expected;
     }
 }
