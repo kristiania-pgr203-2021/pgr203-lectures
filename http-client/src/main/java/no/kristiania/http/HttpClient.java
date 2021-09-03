a5cec516f6dd33bded37f03107f4c9dfaf373220
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-04 00:09:29 +0200

    Update message body

--- http-client/src/main/java/no/kristiania/http/HttpClient.java
+++ http-client/src/main/java/no/kristiania/http/HttpClient.java
@@ -57,19 +57,13 @@ public class HttpClient {
     public int getContentLength() {
         return Integer.parseInt(getHeader("Content-Length"));
     }
-
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket("httpbin.org", 80);
-        socket.getOutputStream().write("GET /html HTTP/1.1\r\nHost: httpbin.org\r\n\r\n".getBytes());
-
-        InputStream in = socket.getInputStream();
-        int c;
-        while ((c = in.read()) != -1) {
-            System.out.print((char)c);
-        }
-    }
-
     public String getMessageBody() {
         return messageBody;
     }
+
+    public static void main(String[] args) throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        System.out.println(client.getMessageBody());
+    }
+
 }
