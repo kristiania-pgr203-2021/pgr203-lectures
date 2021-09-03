a1a95ca94450c113ee3b755ea38ebfddc6c68e73
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-03 22:46:09 +0200

    Making a simple HTTP request

--- http-client/src/main/java/no/kristiania/http/HttpClient.java
+++ http-client/src/main/java/no/kristiania/http/HttpClient.java
@@ -1,9 +1,20 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+        socket.getOutputStream().write("GET /html HTTP/1.1\r\nHost: httpbin.org\r\n\r\n".getBytes());
+
+        InputStream in = socket.getInputStream();
+        int c;
+        while ((c = in.read()) != -1) {
+            System.out.print((char)c);
+        }
     }
     
 }
