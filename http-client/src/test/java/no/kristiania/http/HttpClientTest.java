410ec55bbd340ed8eadb4d5d2c3a5f592f442e15
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-03 22:52:33 +0200

    test shouldGetResponseCode (new failing assertion)

--- http-client/src/test/java/no/kristiania/http/HttpClientTest.java
+++ http-client/src/test/java/no/kristiania/http/HttpClientTest.java
@@ -10,6 +10,8 @@ class HttpClientTest {
     void shouldGetResponseCode() {
         HttpClient client = new HttpClient("httpbin.org", 80, "/status/200");
         assertEquals(200, client.getStatusCode());
+        client = new HttpClient("httpbin.org", 80, "/status/400");
+        assertEquals(400, client.getStatusCode());
     }
 
 }
