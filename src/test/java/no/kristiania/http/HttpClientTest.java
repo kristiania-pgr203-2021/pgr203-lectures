34fbad0f46b6afda8470177a5da3a376aeffad50
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-09 21:06:31 +0200

    Test HttpClient should return status code 🟢

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,11 +2,13 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
     @Test
-    void shouldReturnStatusCode() {
+    void shouldReturnStatusCode() throws IOException {
         assertEquals(200, new HttpClient("httpbin.org", 80, "/html").getStatusCode());
         assertEquals(404, new HttpClient("httpbin.org", 80, "/no-such-file").getStatusCode());
     }
