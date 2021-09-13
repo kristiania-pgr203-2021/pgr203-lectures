1decf3ef51c0e3ecdfc3412bfab7f3ffb65b7c7e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-13 16:26:27 +0200

    Test shouldReturnStatusCode - 404 🟢

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,10 +1,34 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class HttpClient {
-    public HttpClient(String host, int port, String requestTarget) {
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+        String request = "GET " + requestTarget + " HTTP/1.1\r\n" +
+                "Host: " + host + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n";
+        socket.getOutputStream().write(request.getBytes());
+        
+        String[] statusLine = readLine(socket).split(" ");
+        this.statusCode = Integer.parseInt(statusLine[1]);
+    }
+
+    private String readLine(Socket socket) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int c;
+        while ((c = socket.getInputStream().read()) != '\r') {
+            result.append((char)c);
+        }
+        return result.toString();
     }
 
     public int getStatusCode() {
-        return 200;
+        return statusCode;
     }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -3,6 +3,8 @@ package no.kristiania.http;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpClientTest {
@@ -12,7 +14,7 @@ public class HttpClientTest {
     }
 
     @Test
-    void shouldReturnStatusCode() {
+    void shouldReturnStatusCode() throws IOException {
         assertEquals(200, 
                 new HttpClient("httpbin.org", 80, "/html")
                         .getStatusCode());
