3ff135633a3c838a2eafae0594ac89ac8864bcfa
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-13 15:45:23 +0200

    first test 🔴

---
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -0,0 +1,12 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void shouldDoSomething() {
+        assertEquals(200, 100+200);
+    }
+}
