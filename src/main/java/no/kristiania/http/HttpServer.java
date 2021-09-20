8f201d84f1b08a2e4f253b07448c8cd6491ba1fc
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-20 23:52:22 +0200

    HttpServer.main responds to http://localhost:8080/hello

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -45,30 +45,7 @@ public class HttpServer {
     }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-
-
-        Socket clientSocket = serverSocket.accept();
-        
-        
-        String requestLine = HttpClient.readLine(clientSocket);
-
-        System.out.println(requestLine);
-        
-        String headerLine;
-        while (!(headerLine = HttpClient.readLine(clientSocket)).isBlank()) {
-            System.out.println(headerLine);
-        }
-        
-        
-        String messageBody = "Hello world";
-
-        String responseMessage = "HTTP/1.1 200 OK\r\n" +
-                "Content-Length: " + messageBody.length() + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n" +
-                messageBody;
-        clientSocket.getOutputStream().write(responseMessage.getBytes());
+        new HttpServer(8080);
     }
 
     public int getPort() {
