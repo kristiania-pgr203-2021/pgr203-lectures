cccede7e75f8079c502cbf4951d4b3ad7051269b
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-20 23:46:06 +0200

    Test http server should include request target in 404 message 🔴

--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -14,4 +14,11 @@ class HttpServerTest {
         HttpClient client = new HttpClient("localhost", 10001, "/no-such-file");
         assertEquals(404, client.getStatusCode());
     }
+
+    @Test
+    void shouldIncludeRequestTargetIn404Message() throws IOException {
+        HttpServer server = new HttpServer(10002);
+        HttpClient client = new HttpClient("localhost", 10002, "/no-such-file");
+        assertEquals("File not found: /no-such-file", client.getMessageBody());
+    }
 }
