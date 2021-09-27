332f3f28ac461b463d1102894e24faedcf006f61
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-09-27 17:38:01 +0200

    Http server main sets roles

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -100,6 +100,7 @@ public class HttpServer {
 
     public static void main(String[] args) throws IOException {
         HttpServer httpServer = new HttpServer(1962);
+        httpServer.setRoles(List.of("Student", "Teaching assistant", "Teacher"));
         httpServer.setRoot(Paths.get("."));
     }
 
