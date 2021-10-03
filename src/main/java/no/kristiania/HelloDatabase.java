d456562ce6adfb7089c38278a3ab5ed9de72e7c6
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-03 20:51:54 +0200

    Connect to database

--- src/main/java/no/kristiania/HelloDatabase.java
+++ src/main/java/no/kristiania/HelloDatabase.java
@@ -1,7 +1,16 @@
 package no.kristiania;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.sql.SQLException;
+
 public class HelloDatabase {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setURL("jdbc:postgresql://localhost/person_db");
+        dataSource.setUser("person_dbuser");
+        dataSource.setPassword("åneidu!");
+        
+        dataSource.getConnection();
     }
 }
