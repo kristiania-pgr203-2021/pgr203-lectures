9c336f046ea6e7f0f1d9fcaecc5edb4e3045d9ec
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-03 20:53:29 +0200

    Fetch data with JDBC

--- src/main/java/no/kristiania/HelloDatabase.java
+++ src/main/java/no/kristiania/HelloDatabase.java
@@ -2,6 +2,9 @@ package no.kristiania;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class HelloDatabase {
@@ -10,7 +13,17 @@ public class HelloDatabase {
         dataSource.setURL("jdbc:postgresql://localhost/person_db");
         dataSource.setUser("person_dbuser");
         dataSource.setPassword("åneidu!");
-        
-        dataSource.getConnection();
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            try (PreparedStatement statement = connection.prepareStatement("select * from people")) {
+                try (ResultSet rs = statement.executeQuery()) {
+                    while (rs.next()) {
+                        System.out.println(rs.getString("first_name") + " " + rs.getString("last_name"));
+                    }
+                }
+            }
+            
+        }
     }
 }
