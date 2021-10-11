e91fb4f957f673ff5249a21ef362f1cda297c9b1
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-11 15:59:25 +0200

    Person dao should retrieve save person from database 🟢 Adding stuff to database

--- src/test/java/no/kristiania/person/PersonDaoTest.java
+++ src/test/java/no/kristiania/person/PersonDaoTest.java
@@ -1,10 +1,12 @@
 package no.kristiania.person;
 
 import org.junit.jupiter.api.Test;
+import org.postgresql.ds.PGSimpleDataSource;
 
 import javax.sql.DataSource;
 
 import java.sql.SQLException;
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -22,11 +24,23 @@ public class PersonDaoTest {
     }
 
     private Person examplePerson() {
-        return new Person();
+        Person person = new Person();
+        person.setFirstName(pickOne("Johannes", "Juliett", "Jane", "James", "John"));
+        person.setLastName(pickOne("Persson", "Olsen", "Johnson", "Walker"));
+        return person;
     }
+    
+    public static String pickOne(String... alternatives) {
+        return alternatives[new Random().nextInt(alternatives.length)];
+    }
+    
 
     private DataSource createDataSource() {
-        return null;
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/person_db");
+        dataSource.setUser("person_dbuser");
+        dataSource.setPassword("*****");
+        return dataSource;
     }
 
 }
