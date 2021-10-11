f049b253026b1686d0e84ed36eb204d602c20bff
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-11 13:47:46 +0200

    Person dao should retrieve saved person 🟢 but doesn't return from database

--- src/main/java/no/kristiania/people/PersonDao.java
+++ src/main/java/no/kristiania/people/PersonDao.java
@@ -3,7 +3,9 @@ package no.kristiania.people;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PersonDao {
     private final DataSource dataSource;
@@ -15,17 +17,38 @@ public class PersonDao {
 
     public void save(Person person) throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-            try (PreparedStatement statement = connection.prepareStatement("insert into people (first_name, last_name) values (?, ?)")) {
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "insert into people (first_name, last_name) values (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+                )) {
                 statement.setString(1, person.getFirstName());
                 statement.setString(2, person.getLastName());
                 statement.executeUpdate();
+
+                try (ResultSet rs = statement.getGeneratedKeys()) {
+                    rs.next();
+                    person.setId(rs.getLong("id"));
+                }
+                
             }
 
         }
         this.person = person;
     }
 
-    public Person retrieve(Long id) {
-        return person;
+    public Person retrieve(long id) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from people where id = ?")) {
+                statement.setLong(1, id);
+                try (ResultSet rs = statement.executeQuery()) {
+                    rs.next();
+                    Person person = new Person();
+                    person.setFirstName(rs.getString("first_name"));
+                    person.setLastName(rs.getString("last_name"));
+                }
+            }
+        }
+        
+        return this.person;
     }
 }
