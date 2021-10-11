04a2fcfabefe3592009fcc37ba554f655fe0491d
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-11 16:32:53 +0200

    Person dao should list people by last name 🔴

--- src/main/java/no/kristiania/person/PersonDao.java
+++ src/main/java/no/kristiania/person/PersonDao.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class PersonDao {
 
@@ -55,5 +56,9 @@ public class PersonDao {
                 }
             }
         }
+    }
+
+    public List<Person> listByLastName(String lastName) {
+        return null;
     }
 }
--- src/test/java/no/kristiania/person/PersonDaoTest.java
+++ src/test/java/no/kristiania/person/PersonDaoTest.java
@@ -11,16 +11,35 @@ import java.util.Random;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PersonDaoTest {
-    
+
+    private final PersonDao dao = new PersonDao(createDataSource());
+
     @Test
     void shouldRetrieveSavedPersonFromDatabase() throws SQLException {
-        PersonDao dao = new PersonDao(createDataSource());
-        
         Person person = examplePerson();
         dao.save(person);
         assertThat(dao.retrieve(person.getId()))
                 .usingRecursiveComparison()
                 .isEqualTo(person);
+    }
+
+    @Test
+    void shouldListPeopleByLastName() throws SQLException {
+        Person matchingPerson = examplePerson();
+        matchingPerson.setLastName("Testperson");
+        dao.save(matchingPerson);
+        Person anotherMatchingPerson = examplePerson();
+        anotherMatchingPerson.setLastName(matchingPerson.getLastName());
+        dao.save(anotherMatchingPerson);
+
+        Person nonMatchingPerson = examplePerson();
+        dao.save(nonMatchingPerson);
+
+        
+        assertThat(dao.listByLastName(matchingPerson.getLastName()))
+                .extracting(Person::getId)
+                .contains(matchingPerson.getId(), anotherMatchingPerson.getId())
+                .doesNotContain(nonMatchingPerson.getId());
     }
 
     private Person examplePerson() {
