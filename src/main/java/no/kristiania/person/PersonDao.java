fb422fa38ab9fe29365400324fc238b8a20016c8
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-11 16:03:54 +0200

    Person dao should retrieve save person from database 🔴 Fails because person.getId return null

--- src/main/java/no/kristiania/person/PersonDao.java
+++ src/main/java/no/kristiania/person/PersonDao.java
@@ -3,6 +3,7 @@ package no.kristiania.person;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PersonDao {
@@ -29,7 +30,24 @@ public class PersonDao {
         this.person = person;
     }
 
-    public Person retrieve(Long id) {
-        return person;
+    public Person retrieve(Long id) throws SQLException {
+
+        try (Connection connection = dataSource.getConnection()) {
+
+            try (PreparedStatement statement = connection.prepareStatement(
+                    "select * from people where id = ?"
+            )) {
+                statement.setLong(1, id);
+
+                try (ResultSet rs = statement.executeQuery()) {
+                    rs.next();
+                    
+                    Person person = new Person();
+                    person.setFirstName(rs.getString("first_name"));
+                    person.setLastName(rs.getString("last_name"));
+                    return person;
+                }
+            }
+        }
     }
 }
