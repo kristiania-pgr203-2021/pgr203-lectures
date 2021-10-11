35d7e7b3e799cb32a87b9e8356d002c7304f435a
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-11 13:34:45 +0200

    Person dao should retrieve saved person 🟢 passes stupidly

--- src/main/java/no/kristiania/people/PersonDao.java
+++ src/main/java/no/kristiania/people/PersonDao.java
@@ -3,15 +3,18 @@ package no.kristiania.people;
 import javax.sql.DataSource;
 
 public class PersonDao {
+    private Person person;
+
     public PersonDao(DataSource dataSource) {
         
     }
 
     public void save(Person person) {
-        
+
+        this.person = person;
     }
 
     public Person retrieve(Long id) {
-        return null;
+        return person;
     }
 }
