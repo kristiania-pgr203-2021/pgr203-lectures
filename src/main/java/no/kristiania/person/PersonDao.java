e3ae7fba6cae0061770cf04225c3f1bd90ab3f05
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-11 16:47:26 +0200

    UNDO Create SQL injection exploit

--- src/main/java/no/kristiania/person/PersonDao.java
+++ src/main/java/no/kristiania/person/PersonDao.java
@@ -75,8 +75,9 @@ public class PersonDao {
     public List<Person> listByLastName(String lastName) throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
             try (PreparedStatement statement = connection.prepareStatement(
-                    "select * from people where last_name = '" + lastName + "'"
+                    "select * from people where last_name = ?"
             )) {
+                statement.setString(1, lastName);
                 try (ResultSet rs = statement.executeQuery()) {
                     ArrayList<Person> people = new ArrayList<>();
 
