7c4ffb34bd541dd373de6ea5029fc609a1c6fbc7
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-16 17:20:24 +0200

    Read database password from properties-file

--- src/main/java/no/kristiania/person/PersonnelServer.java
+++ src/main/java/no/kristiania/person/PersonnelServer.java
@@ -7,7 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Properties;
 
 public class PersonnelServer {
     private static final Logger logger = LoggerFactory.getLogger(PersonnelServer.class);
@@ -23,11 +25,16 @@ public class PersonnelServer {
         logger.info("Started server on http://localhost:{}/index.html", server.getPort());
     }
 
-    private static DataSource createDataSource() {
+    private static DataSource createDataSource() throws IOException {
+        Properties properties = new Properties();
+        try (FileReader reader = new FileReader("pgr203.properties")) {
+            properties.load(reader);
+        }
+
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/person_db");
-        dataSource.setUser("person_dbuser");
-        dataSource.setPassword("*****");
+        dataSource.setUrl(properties.getProperty("dataSource.url", "jdbc:postgresql://localhost:5432/person_db"));
+        dataSource.setUser(properties.getProperty("dataSource.user", "person_dbuser"));
+        dataSource.setPassword(properties.getProperty("dataSource.password"));
         Flyway.configure().dataSource(dataSource).load().migrate();
         return dataSource;
     }
