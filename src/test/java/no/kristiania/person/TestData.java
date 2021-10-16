7e6bb2e7e8c0184429412cdcd06b8575a8d9cf7e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-16 15:48:25 +0200

    Test: Role DAO should list saved roles 🔴 (missing table)

--- src/test/java/no/kristiania/person/TestData.java
+++ src/test/java/no/kristiania/person/TestData.java
@@ -1,9 +1,13 @@
 package no.kristiania.person;
 
+import org.h2.jdbcx.JdbcDataSource;
+
 import javax.sql.DataSource;
 
 public class TestData {
     public static DataSource createDataSource() {
-        return null;
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:testDatabase");
+        return dataSource;
     }
 }
