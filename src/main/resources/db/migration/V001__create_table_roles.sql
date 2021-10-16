62ea5dd51a7a644b5fe1bdc1e08d1a700e32e14e
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-16 15:51:59 +0200

    Test: Role DAO should list saved roles 🟢

---
+++ src/main/resources/db/migration/V001__create_table_roles.sql
@@ -0,0 +1,5 @@
+CREATE TABLE roles
+(
+    id   SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
--- src/test/java/no/kristiania/person/TestData.java
+++ src/test/java/no/kristiania/person/TestData.java
@@ -1,5 +1,6 @@
 package no.kristiania.person;
 
+import org.flywaydb.core.Flyway;
 import org.h2.jdbcx.JdbcDataSource;
 
 import javax.sql.DataSource;
@@ -7,7 +8,8 @@ import javax.sql.DataSource;
 public class TestData {
     public static DataSource createDataSource() {
         JdbcDataSource dataSource = new JdbcDataSource();
-        dataSource.setUrl("jdbc:h2:mem:testDatabase");
+        dataSource.setUrl("jdbc:h2:mem:testDatabase;DB_CLOSE_DELAY=-1");
+        Flyway.configure().dataSource(dataSource).load().migrate();
         return dataSource;
     }
 }
