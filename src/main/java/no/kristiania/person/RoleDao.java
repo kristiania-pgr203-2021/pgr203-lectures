1d665f2111be200561a79cc465ec022e68328241
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-18 16:25:03 +0200

    RoleDaoTest should list saved roles 🔴 (compiles, but fails)

---
+++ src/main/java/no/kristiania/person/RoleDao.java
@@ -0,0 +1,18 @@
+package no.kristiania.person;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class RoleDao {
+    public RoleDao(DataSource dataSource) {
+        
+    }
+
+    public void save(String roleName) {
+        
+    }
+
+    public List<String> listAll() {
+        return null;
+    }
+}
--- src/test/java/no/kristiania/person/RoleDaoTest.java
+++ src/test/java/no/kristiania/person/RoleDaoTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RoleDaoTest {
     
     private RoleDao dao = new RoleDao(TestData.testDataSource());
