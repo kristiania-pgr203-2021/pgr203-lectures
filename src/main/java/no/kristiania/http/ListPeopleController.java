f185a8ff73126d20a978c23213090525a0206509
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-10-25 17:01:44 +0200

    Http server should list people from database 🟢

--- src/main/java/no/kristiania/http/ListPeopleController.java
+++ src/main/java/no/kristiania/http/ListPeopleController.java
@@ -1,15 +1,25 @@
 package no.kristiania.http;
 
+import no.kristiania.person.Person;
 import no.kristiania.person.PersonDao;
 
 import java.sql.SQLException;
 
 public class ListPeopleController implements HttpController {
+    private final PersonDao personDao;
+
     public ListPeopleController(PersonDao personDao) {
+        this.personDao = personDao;
     }
 
     @Override
     public HttpMessage handle(HttpMessage request) throws SQLException {
-        return new HttpMessage("HTTP/1.1 200 OK", "OK");
+        String response = "";
+
+        for (Person person : personDao.listAll()) {
+            response += "<div>" + person.getLastName() + ", " + person.getFirstName() + "</div>";
+        }
+
+        return new HttpMessage("HTTP/1.1 200 OK", response);
     }
 }
